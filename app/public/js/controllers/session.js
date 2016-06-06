bb08942312551369288c489902bb8e812916af66
gorco <antcal01@ucm.es>
2016-06-06 10:51:34 +0200

    Added kibana path

--- app/public/js/controllers/session.js
+++ app/public/js/controllers/session.js
@@ -68,7 +68,7 @@ angular.module('sessionApp', ['myApp', 'ngStorage', 'services'])
 
             $scope.dashboardLink = function() {
                 var url = CONSTANTS.KIBANA+'/app/kibana#/dashboard/dashboard_' +
-                    QueryParams.getQueryParam('session') + '?embed=true_g=(refreshInterval:' +
+                QueryParams.getQueryParam('session') + '?embed=true_g=(refreshInterval:' +
                     '(display:Off,pause:!f,value:0),time:(from:now-5y,mode:quick,to:now))';
                 if(url.startsWith('localhost')){
                     url = 'http://'+url;
